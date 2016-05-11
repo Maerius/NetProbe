755db5ec3885e2289a24fa5a4572c8453fce3e27
Maerius <Philipmattsson1@gmail.com>
2016-05-11 07:00:13 +0000

    Added timeout to DHCP test

--- dhcptest.sh
+++ dhcptest.sh
@@ -6,9 +6,9 @@ logfile=$DIR/logging.log
 echo "============ Start ============" >> $logfile
 log=`echo $(date -u) >> $logfile`
 echo "===============================" >> $logfile
-dhcping=`\time -f "Time test 1: \t%E elapsed" -o $logfile -a sudo $DIR/dhtest -m 00:0c:29:bc:fd:f6 -L 1 -V >> $logfile`
-dhcping=`\time -f "Time test 2: \t%E elapsed" -o $logfile -a sudo $DIR/dhtest -m 00:0c:29:bc:fd:f7 -L 1 -V >> $logfile`
-dhcping=`\time -f "Time test 3: \t%E elapsed" -o $logfile -a sudo $DIR/dhtest -m 00:0c:29:bc:fd:f8 -L 1 -V >> $logfile`
+dhcping=`\time -f "Time test 1: \t%E elapsed" -o $logfile -a sudo $DIR/dhtest -T 20 -m 00:0c:29:bc:fd:f6 -L 1 -V >> $logfile`
+dhcping=`\time -f "Time test 2: \t%E elapsed" -o $logfile -a sudo $DIR/dhtest -T 20 -m 00:0c:29:bc:fd:f7 -L 1 -V >> $logfile`
+dhcping=`\time -f "Time test 3: \t%E elapsed" -o $logfile -a sudo $DIR/dhtest -T 20 -m 00:0c:29:bc:fd:f8 -L 1 -V >> $logfile`
 echo "============= End ==============" >> $logfile 
 echo " " >> $logfile
 rm -rf 00:0c:29:bc:fd:f*
