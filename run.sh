31ce2d4a7a3f541b07542fff6aeee45fd4b16bd8
Maerius <Philipmattsson1@gmail.com>
2016-05-11 13:16:30 +0000

    Getting 'Done'. Fixed a lot of stuff.

--- run.sh
+++ run.sh
@@ -5,21 +5,22 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/pi/NetPr
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 datedir=`date '+%d-%m-%Y_%H:00'`
-mkdir $DIR/$datedir
-
-portactive=`cat /sys/class/net/eth0/carrier` 
-alreadyrun=`cat $DIR/alrun`
+#mkdir $DIR/$datedir
 
 while :
 do
+
+portactive=`cat /sys/class/net/eth0/carrier`
+alreadyrun=`cat $DIR/alrun`
+alrunfile=$DIR/alrun
+
 if [ $portactive = 0 ]; then
-echo "0" > $alreadyrun
+echo "0" > $alrunfile
 fi
 
 if [ $alreadyrun = 0 ]; then
-
-	if [ $portactive = 1 -a $alreadyrun = 0 ]; then
-
+	if [ $portactive = 1 ] && [ $alreadyrun = 0 ]; then
+		echo "1" > $alrunfile
 		echo "Running PING  Test"
 		echo "Running DHCP  Test" `$DIR/pingtest.sh`
 		echo "Running DNS   Test" `$DIR/dhcptest.sh`
@@ -27,9 +28,7 @@ if [ $alreadyrun = 0 ]; then
 		echo "Running SPEED Test" `$DIR/sitetest.sh`
 		echo "Running CDP   Test" `$DIR/extspeed.sh`
 		echo "All done!" `$DIR/cdptest.sh` 
-	
 	fi
-else
-echo "Already run this script once."
+echo "End"
 fi
 done
