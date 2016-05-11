d88031976cf0057fd5aadb80a36ec6455e7b0f8c
Maerius <aerius@it.pe>
2016-05-11 15:49:29 +0200

    Added directory check

--- run.sh
+++ run.sh
@@ -5,10 +5,14 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/pi/NetPr
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 datedir=`date '+%d-%m-%Y_%H:00'`
-mkdir $DIR/$datedir
 
+md=$DIR/$datedir
 while :
 do
+
+if [ ! -d $md ]; then
+        mkdir $md
+fi
 
 portactive=`cat /sys/class/net/eth0/carrier`
 alreadyrun=`cat $DIR/alrun`
@@ -19,16 +23,16 @@ echo "0" > $alrunfile
 fi
 
 if [ $alreadyrun = 0 ]; then
-	if [ $portactive = 1 ] && [ $alreadyrun = 0 ]; then
-		echo "1" > $alrunfile
-		sleep 10;
-		echo "Running PING  Test"
-		echo "Running DHCP  Test" `$DIR/pingtest.sh`
-		echo "Running DNS   Test" `$DIR/dhcptest.sh`
-		echo "Running SITE  Test" `$DIR/dnstest.sh`
-		echo "Running SPEED Test" `$DIR/sitetest.sh`
-		echo "Running CDP   Test" `$DIR/extspeed.sh`
-		echo "All done!" `$DIR/cdptest.sh` 
-	fi
+        if [ $portactive = 1 ] && [ $alreadyrun = 0 ]; then
+                echo "1" > $alrunfile
+                sleep 10;
+                echo "Running PING  Test"
+                echo "Running DHCP  Test" `$DIR/pingtest.sh`
+                echo "Running DNS   Test" `$DIR/dhcptest.sh`
+                echo "Running SITE  Test" `$DIR/dnstest.sh`
+                echo "Running SPEED Test" `$DIR/sitetest.sh`
+                echo "Running CDP   Test" `$DIR/extspeed.sh`
+                echo "All done!" `$DIR/cdptest.sh`
+        fi
 fi
 done
